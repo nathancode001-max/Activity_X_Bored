ef5f5e63c6695168156d7f47913af76aaf901ddc
nathancode001 <nathancode001@gmail.com>
2026-06-22 17:41:45 +0100

    feat: enhance frontend to handle user input for activity type and participants

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,6 +2,9 @@ const activity = document.getElementById("activity");
 const type = document.getElementById("type");
 const participants = document.getElementById("participants");
 
+const submit_btn = document.getElementById("submit_btn");
+const option_type = document.getElementById("option_type");
+const option_participants = document.getElementById("option_participants");
 
 
 
@@ -33,10 +36,35 @@ async function getActivities() {
             participants.textContent = `👥 Participants: ${data.participants}`;
         }
     } catch (error) {
-        console.log(error);
+        console.error(error);
     };
     
 };
 
 
 getActivities();
+
+submit_btn.addEventListener("click", async () => {
+
+
+   const userInput =  {type: option_type.value, participants: option_participants.value};
+
+    try {
+
+        const res = await fetch(`${url}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userInput)
+    });
+
+    if(!res.ok){
+        console.log("okay")
+    }
+        
+    } catch (error) {
+        console.error(error);
+    }
+
+});
