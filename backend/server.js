60d688620400cb392d436ea4de55fd6513b6e925
nathancode001 <nathancode001@gmail.com>
2026-06-22 21:34:50 +0100

    feat: implement POST endpoint to filter activities by type and participants

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,12 @@ const port = 3000;
 
 app.use(cors());
 
+// For parsing application/json
+app.use(express.json());
+
+// For parsing application/x-www-form-urlencoded (HTML form submissions)
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", async (req, res) => {
     try {
         const response  = await axios.get("https://bored-api.appbrewery.com/random");
@@ -20,6 +26,31 @@ app.get("/", async (req, res) => {
         console.error("Failed to make request:", error.message);
         res.status(500).json({error: error.message});
     }
+});
+
+
+
+app.post("/", async (req, res) =>{
+
+    const type = req.body.type
+    const participants = req.body.participants
+
+    if (type || participants){
+            try {
+                const response  = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
+                const data = response.data;
+                const randomValue = data[ Math.floor(Math.random() * data.length)]
+                res.json(randomValue); 
+            } catch (error) {
+                console.error("Failed to make request:", error.message); 
+                res.status(500).json({error: error.message}); 
+            }
+            return
+    }else{
+        res.status(404).json({message: "you did not provide the correct body property"})
+    }
+
+
 });
 
 
